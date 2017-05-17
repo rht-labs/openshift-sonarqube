58fada086d68bf2bd1fab7432d432c74480c4693
Deven Phillips <deven.phillips@gmail.com>
2017-05-17 11:25:23 -0400

    More permissions fixes

--- run.sh
+++ run.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-## If the mounted extensions volume is empty, populate it from the default data
-if [[ "$(ls -A /opt/sonarqube/extensions)" ]]; then
-	cp -a /opt/sonarqube/extensions-init /opt/sonarqube/extensions
-fi
-
 ## If the mounted data volume is empty, populate it from the default data
-if [[ "$(ls -A /opt/sonarqube/data)" ]]; then
+if ! [[ "$(ls -A /opt/sonarqube/data)" ]]; then
 	cp -a /opt/sonarqube/data-init /opt/sonarqube/data
 fi
+
+## If the mounted extensions volume is empty, populate it from the default data
+if ! [[ "$(ls -A /opt/sonarqube/data/plugins)" ]]; then
+	cp -a /opt/sonarqube/extensions-init /opt/sonarqube/data/plugins
+fi
+
+rm -rf /opt/sonarqube/extensions/plugins
+ln -s /opt/sonarqube/data/plugins /opt/sonarqube/extensions/plugins
 
 if [ "${1:0:1}" != '-' ]; then
   exec "$@"
