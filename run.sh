babae942ea10bd94c4758c4df40a1bd024863816
Deven Phillips <deven.phillips@gmail.com>
2017-05-23 15:58:19 -0400

    Debugging SonarQube properties

--- run.sh
+++ run.sh
@@ -22,6 +22,8 @@ fi
 
 #chown -R 0:0 /opt/sonarqube && chmod -R g+rwX /opt/sonarqube
 
+set
+
 java -jar lib/sonar-application-$SONAR_VERSION.jar \
     -Dsonar.web.javaAdditionalOpts="${env:SONARQUBE_WEB_JVM_OPTS} -Djava.security.egd=file:/dev/./urandom" \
     "$@"
