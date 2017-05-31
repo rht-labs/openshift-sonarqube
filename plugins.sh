9abbcfca951168530bb077db0483fcb7d7079a91
Deven Phillips <deven.phillips@gmail.com>
2017-05-30 21:36:06 -0400

    Debugging trace

--- plugins.sh
+++ plugins.sh
@@ -5,7 +5,7 @@ set -x
 printf 'Updating CA Certs\n'
 /usr/sbin/update-ca-certificates
 printf 'Downloading plugin details\n'
-curl -k -L -sS https://update.sonarsource.org/update-center.properties -o /tmp/pluginList.txt
+curl -k -L --trace https://update.sonarsource.org/update-center.properties -o /tmp/pluginList.txt
 printf "Downloading additional plugins\n"
 for PLUGIN in "$@"
 do
@@ -15,7 +15,7 @@ do
     ## Check to see if plugin exists, attempt to download the plugin if it does exist.
     if ! [[ -z "${DOWNLOAD_URL}" ]]; then
         printf "\t\t%-15s" ${PLUGIN}
-        curl -k -L -sS -o /opt/sonarqube/extensions-init/plugins/${PLUGIN}.jar ${DOWNLOAD_URL} && printf "%10s" "DONE" || printf "%10s" "FAILED"
+        curl -k -L --trace -o /opt/sonarqube/extensions-init/plugins/${PLUGIN}.jar ${DOWNLOAD_URL} && printf "%10s" "DONE" || printf "%10s" "FAILED"
         printf "\n"
     else
         ## Plugin was not found in the plugin inventory
