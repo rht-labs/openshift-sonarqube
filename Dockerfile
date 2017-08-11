d683a15065dddcba003c260c5472d90c5d36ce14
Deven Phillips <deven.phillips@redhat.com>
2017-08-11 13:39:51 -0400

    Debugging SSL/OCP issues

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM sonarqube:latest
 MAINTAINER Deven Phillips <deven.phillips@redhat.com>
 
+USER root
 ADD sonar.properties /opt/sonarqube/conf/sonar.properties
 ADD run.sh /opt/sonarqube/bin/run.sh
-CMD /opt/sonarqube/bin/run.sh
 RUN cp -a /opt/sonarqube/data /opt/sonarqube/data-init && \
-	cp -a /opt/sonarqube/extensions /opt/sonarqube/extensions-init && \
-	chown 65534:0 /opt/sonarqube && chmod -R gu+rwX /opt/sonarqube
+	cp -a /opt/sonarqube/extensions /opt/sonarqube/extensions-init
 ADD plugins.sh /opt/sonarqube/bin/plugins.sh
 RUN /opt/sonarqube/bin/plugins.sh pmd gitlab github ldap
+RUN chown root:root /opt/sonarqube -R
+RUN chmod 775 /opt/sonarqube -R
--- run.sh
+++ run.sh
@@ -3,6 +3,17 @@
 set -x
 set -e
 
+if ! [[ "${CA_CERT_URL}X" == "X" ]]; then
+    curl "${CA_CERT_URL}" > /tmp/ca_cert
+fi
+
+cp /etc/ssl/certs/java/cacerts /opt/sonarqube/cacerts
+chmod 775 /opt/sonarqube/cacerts
+
+if [[ -f /tmp/ca_cert ]]; then
+    keytool -importcert -keystore /opt/sonarqube/cacerts -storepass changeit -alias ocp_root -file /tmp/ca_cert
+fi
+
 ## If the mounted data volume is empty, populate it from the default data
 if ! [[ "$(ls -A /opt/sonarqube/data)" ]]; then
     cp -a /opt/sonarqube/data-init /opt/sonarqube/data
@@ -21,10 +32,7 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-#chown -R 0:0 /opt/sonarqube && chmod -R g+rwX /opt/sonarqube
-
-set
-
 java -jar lib/sonar-application-$SONAR_VERSION.jar \
+    -Djavax.net.ssl.trustStore=/opt/sonarqube/cacerts -Djavax.net.ssl.trustStorePassword=changeit \
     -Dsonar.web.javaAdditionalOpts="${SONARQUBE_WEB_JVM_OPTS} -Djava.security.egd=file:/dev/./urandom" \
     "$@"
